eaa67af14c45d5b57034c789e02cd583b9115bb0
mgrant <michael.grant@youview.com>
2021-03-23 18:29:10 +0000

    support conjured items

--- clojure/spec/gilded_rose/core_spec.clj
+++ clojure/spec/gilded_rose/core_spec.clj
@@ -1,6 +1,60 @@
 (ns gilded-rose.core-spec
 (:require [clojure.test :refer :all]
-          [gilded-rose.core :refer [update-quality item]]))
+          [gilded-rose.core :refer :all]))
 
-(deftest gilded-rose-test
-  (is (= "fixme" (:name (first (update-quality [(item "foo" 0 0)]))))))
+(deftest normal-item
+
+  (are [input days expected]
+    (= expected (update-current-inventory input days))
+    [(item "+5 Dexterity Vest" 10 20)] 1 [(item "+5 Dexterity Vest" 9 19)]
+    [(item "+5 Dexterity Vest" 1 20)] 1 [(item "+5 Dexterity Vest" 0 19)]
+    [(item "+5 Dexterity Vest" 0 20)] 1 [(item "+5 Dexterity Vest" -1 19)]
+    [(item "+5 Dexterity Vest" -1 20)] 1 [(item "+5 Dexterity Vest" -2 18)]
+
+    [(item "+5 Dexterity Vest" 10 1)] 1 [(item "+5 Dexterity Vest" 9 0)]
+    [(item "+5 Dexterity Vest" 10 0)] 1 [(item "+5 Dexterity Vest" 9 0)]
+    [(item "+5 Dexterity Vest" 10 0)] 2 [(item "+5 Dexterity Vest" 8 0)]))
+
+(deftest aged-brie
+  (are [input days expected]
+    (= expected (update-current-inventory input days))
+    [(item "Aged Brie" 2 0)] 1 [(item "Aged Brie" 1 1)]
+    [(item "Aged Brie" 2 0)] 2 [(item "Aged Brie" 0 2)]
+    [(item "Aged Brie" 2 0)] 3 [(item "Aged Brie" -1 3)]
+    [(item "Aged Brie" 2 0)] 23 [(item "Aged Brie" -21 23)]
+    [(item "Aged Brie" 2 0)] 223 [(item "Aged Brie" -221 50)]))
+
+(deftest sulfuras
+  (are [input days expected]
+    (= expected (update-current-inventory input days))
+    [(item "Sulfuras, Hand of Ragnaros" 1 80)] 1 [(item "Sulfuras, Hand of Ragnaros" 1 80)]
+    [(item "Sulfuras, Hand of Ragnaros" 0 80)] 1 [(item "Sulfuras, Hand of Ragnaros" 0 80)]
+    [(item "Sulfuras, Hand of Ragnaros" 0 80)] 2 [(item "Sulfuras, Hand of Ragnaros" 0 80)]
+    [(item "Sulfuras, Hand of Ragnaros" 0 80)] 200 [(item "Sulfuras, Hand of Ragnaros" 0 80)]))
+
+(deftest backstage-passes
+  (are [input days expected]
+    (= expected (update-current-inventory input days))
+    [(item "Backstage passes to a TAFKAL80ETC concert" 15 1)] 5 [(item "Backstage passes to a TAFKAL80ETC concert" 10 6)]
+    [(item "Backstage passes to a TAFKAL80ETC concert" 10 1)] 5 [(item "Backstage passes to a TAFKAL80ETC concert" 5 11)]
+    [(item "Backstage passes to a TAFKAL80ETC concert" 5 1)] 5 [(item "Backstage passes to a TAFKAL80ETC concert" 0 16)]
+    [(item "Backstage passes to a TAFKAL80ETC concert" 5 1)] 6 [(item "Backstage passes to a TAFKAL80ETC concert" -1 0)]
+    [(item "Backstage passes to a TAFKAL80ETC concert" 5 1)] 7 [(item "Backstage passes to a TAFKAL80ETC concert" -2 0)]
+
+    [(item "Backstage passes to a TAFKAL80ETC concert" 15 49)] 1 [(item "Backstage passes to a TAFKAL80ETC concert" 14 50)]
+    [(item "Backstage passes to a TAFKAL80ETC concert" 15 49)] 2 [(item "Backstage passes to a TAFKAL80ETC concert" 13 50)]
+    [(item "Backstage passes to a TAFKAL80ETC concert" 15 40)] 14 [(item "Backstage passes to a TAFKAL80ETC concert" 1 50)]))
+
+(deftest conjured-items
+  (are [input days expected]
+    (= expected (update-current-inventory input days))
+    [(item "Conjured bread" 10 20)] 1 [(item "Conjured bread" 9 18)]
+    [(item "Conjured bread" 1 20)] 1 [(item "Conjured bread" 0 18)]
+    [(item "Conjured bread" 0 20)] 1 [(item "Conjured bread" -1 18)]
+    [(item "Conjured bread" -1 20)] 1 [(item "Conjured bread" -2 16)]
+    [(item "Conjured bread" -1 20)] 2 [(item "Conjured bread" -3 12)]
+
+    [(item "Conjured bread" 10 2)] 1 [(item "Conjured bread" 9 0)]
+    [(item "Conjured bread" 10 1)] 1 [(item "Conjured bread" 9 0)]
+    [(item "Conjured bread" 10 0)] 1 [(item "Conjured bread" 9 0)]
+    [(item "Conjured bread" 10 0)] 2 [(item "Conjured bread" 8 0)]))
--- clojure/src/gilded_rose/core.clj
+++ clojure/src/gilded_rose/core.clj
@@ -1,44 +1,70 @@
 (ns gilded-rose.core)
 
-(defn update-quality [items]
-  (map
-    (fn[item] (cond
-      (and (< (:sell-in item) 0) (= "Backstage passes to a TAFKAL80ETC concert" (:name item)))
-        (merge item {:quality 0})
-      (or (= (:name item) "Aged Brie") (= (:name item) "Backstage passes to a TAFKAL80ETC concert"))
-        (if (and (= (:name item) "Backstage passes to a TAFKAL80ETC concert") (>= (:sell-in item) 5) (< (:sell-in item) 10))
-          (merge item {:quality (inc (inc (:quality item)))})
-          (if (and (= (:name item) "Backstage passes to a TAFKAL80ETC concert") (>= (:sell-in item) 0) (< (:sell-in item) 5))
-            (merge item {:quality (inc (inc (inc (:quality item))))})
-            (if (< (:quality item) 50)
-              (merge item {:quality (inc (:quality item))})
-              item)))
-      (< (:sell-in item) 0)
-        (if (= "Backstage passes to a TAFKAL80ETC concert" (:name item))
-          (merge item {:quality 0})
-          (if (or (= "+5 Dexterity Vest" (:name item)) (= "Elixir of the Mongoose" (:name item)))
-            (merge item {:quality (- (:quality item) 2)})
-            item))
-      (or (= "+5 Dexterity Vest" (:name item)) (= "Elixir of the Mongoose" (:name item)))
-        (merge item {:quality (dec (:quality item))})
-      :else item))
-  (map (fn [item]
-      (if (not= "Sulfuras, Hand of Ragnaros" (:name item))
-        (merge item {:sell-in (dec (:sell-in item))})
-        item))
-  items)))
-
 (defn item [item-name, sell-in, quality]
   {:name item-name, :sell-in sell-in, :quality quality})
 
-(defn update-current-inventory[]
-  (let [inventory 
-    [
-      (item "+5 Dexterity Vest" 10 20)
-      (item "Aged Brie" 2 0)
-      (item "Elixir of the Mongoose" 5 7)
-      (item "Sulfuras, Hand Of Ragnaros" 0 80)
-      (item "Backstage passes to a TAFKAL80ETC concert" 15 20)
-    ]]
-    (update-quality inventory)
-    ))
+(def inventory
+  [(item "+5 Dexterity Vest" 10 20)
+   (item "Aged Brie" 2 0)
+   (item "Elixir of the Mongoose" 5 7)
+   (item "Sulfuras, Hand Of Ragnaros" 0 80)
+   (item "Backstage passes to a TAFKAL80ETC concert" 15 20)])
+
+(defn aged-brie? [item] (= (:name item) "Aged Brie"))
+(defn sulfuras? [item] (= (:name item) "Sulfuras, Hand of Ragnaros"))
+(defn backstage-pass? [item] (clojure.string/starts-with? (:name item) "Backstage pass"))
+(defn conjured? [item] (clojure.string/starts-with? (:name item) "Conjured"))
+
+(defn update-quality [quality increase amount]
+  (if increase
+    (min 50 (+ quality amount))
+    (max 0 (- quality amount))))
+
+(defn update-aged-brie [item]
+  (-> item
+      (update :quality #(update-quality % true 1))
+      (update :sell-in dec)))
+
+(defn update-backstage-pass [item]
+  (-> item
+      (update :quality #(cond
+                          (< 10 (:sell-in item)) (update-quality % true 1)
+                          (< 5 (:sell-in item)) (update-quality % true 2)
+                          (< 0 (:sell-in item)) (update-quality % true 3)
+                          :else 0))
+      (update :sell-in dec)))
+
+(defn update-sulfuras [item]
+  item)
+
+(defn update-conjured [item]
+  (-> item
+      (update :quality #(if (neg? (:sell-in item))
+                          (update-quality % false 4)
+                          (update-quality % false 2)))
+      (update :sell-in dec)))
+
+(defn update-normal [item]
+  (-> item
+      (update :quality #(if (neg? (:sell-in item))
+                          (update-quality % false 2)
+                          (update-quality % false 1)))
+      (update :sell-in dec)))
+
+(defn update-item [item]
+  (cond
+    (aged-brie? item) (update-aged-brie item)
+    (sulfuras? item) (update-sulfuras item)
+    (backstage-pass? item) (update-backstage-pass item)
+    (conjured? item) (update-conjured item)
+    :else (update-normal item)))
+
+(defn update-inventory [items]
+  (map update-item items))
+
+(defn update-current-inventory[inventory days]
+  (loop [days days
+         inventory inventory]
+    (if-not (pos? days)
+      inventory
+      (recur (dec days) (update-inventory inventory)))))
